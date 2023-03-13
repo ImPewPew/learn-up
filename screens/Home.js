fcf721c7e4e8eb8ba71449db8fc4270774ff8dab
ImPewPew <eman2_barcelo@yahoo.com>
2023-03-13 11:21:17 +0800

    First Commit

---
+++ screens/Home.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+
+
+
+const Home = ({navigation}) => {
+    return (
+
+    );
+};
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+});
